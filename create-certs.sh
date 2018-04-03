9f5897a7f4bcc7c07bc4dbb71051394d8bcaeaef
david@david-mbp <david@davidjenni.com>
2018-04-02 22:24:09 -0700

    use node DNS names only; protect key files

--- create-certs.sh
+++ create-certs.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # see: https://coreos.com/os/docs/latest/generate-self-signed-certificates.html
 
-cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
+cfssl gencert -initca ca.json | cfssljson -bare ca -
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare server
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare client
 rm *.csr
+chmod 0400 *-key.pem
 
 # rename client cert files to what docker expects:
 mv client.pem cert.pem
